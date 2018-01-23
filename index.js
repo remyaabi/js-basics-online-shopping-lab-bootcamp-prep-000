16704158727c3cdf680a3d0a3265687cfd07d1ee
Remya Jacob <remyaabi@gmail.com>
2018-01-23 23:04:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -115,5 +115,8 @@ function placeOrder(cardNumber) {
     message="Sorry, we don't have a credit card on file for you.";
   }else{
     message="Your total cost is $" +total()+", which will be charged to the card " + cardNumber+".";
+      var c=[];
+    var cart=setCart(c) 
   }
+  return message;
 }
